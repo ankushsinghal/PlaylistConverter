a1f698f5b3195c9bdd74a2e26ceb57b8943c70b1
Ankush Singhal <ankushsinghal1995@gmail.com>
2019-07-23 10:58:36 +0530

    Added code to create Youtube playlist
    Added code to create a Youtube Playlist with the same name as the Spotify playlist

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var exphbs = require('express-handlebars');
 var credentials = require('./credentials');
 
 var global_spotify_access_token;
+var global_youtube_access_token;
+var global_playlist_name;
 
 /**
  * Generates a random string containing numbers and letters
@@ -129,12 +131,93 @@ app.get('/playlist/:playlistId-:playlistName', function(req, res){
     json: true
   };
 
+  global_playlist_name = req.params.playlistName;
+
   request.get(playlistOptions, function(error, response, body){
     //console.log(body);
     var trackList = body.tracks.items;
     console.log(trackList);
-  });
 
+    var state = generateRandomString(16);
+    var scope = 'https://www.googleapis.com/auth/youtube';
+
+    res.redirect('https://accounts.google.com/o/oauth2/v2/auth?' + 
+                  querystring.stringify({
+                    client_id: credentials.youtube_client_id,
+                    redirect_uri: credentials.youtube_redirect_uri,
+                    scope: scope,
+                    access_type: 'offline',
+                    state: state,
+                    response_type: 'code'
+                  }));
+  });
+});
+
+app.get('/youtube/callback', function(req,res){
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+  if (state === null) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  }
+  else {
+    var authOptions = {
+      url: 'https://www.googleapis.com/oauth2/v4/token',
+      form: {
+        code: code,
+        redirect_uri: credentials.youtube_redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(credentials.youtube_client_id + ':' + credentials.youtube_client_secret).toString('base64'))
+      },
+      json: true
+    };
+    console.log(authOptions);
+    request.post(authOptions, function(error, response, body){
+      if(!error && response.statusCode == 200){
+        var access_token = body.access_token;
+        var refresh_token = body.refresh_token;
+
+        global_youtube_access_token = access_token;
+        console.log(global_youtube_access_token);
+
+        var playlistAuthOptions = {
+          url: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet,status',
+          body: {
+            snippet: {
+              title: global_playlist_name,
+            },
+            status: {
+              privacyStatus: 'public'
+            }
+          },
+          headers: {
+            'Authorization': 'Bearer ' + global_youtube_access_token
+          },
+          json: true
+        };
+        console.log(playlistAuthOptions);
+        request.post(playlistAuthOptions, function(error, response, body){
+          console.log(error);
+          console.log(response);
+          console.log(body);
+          if(!error && response.statusCode == 200){
+            console.log('PLAYLIST SHOULD NOW BE CREATED');
+          }
+          else{
+            console.log('ERROR 2');
+          }
+        });
+      }
+      else{
+        console.log('ERROR');
+      }
+    });
+  }
 });
 
 console.log('Listening on 8888');
